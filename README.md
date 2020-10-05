16c5766d6324eabe3b062daff00f057caab38c81
Ian Wessen <ian@honeybook.com>
2020-10-05 15:09:53 -0700

    cra client and basic crud api

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ Start the server with `nodemon server/index.js`
 
 ## Postgres
 
-For this project, there is a database called `nought` (may or may not exist currently).
+For this project, we work in a database called `nought`.
 
-Start interacting with the database through the command line, start a session with `psql -U ianwessen -d nought`
+Start interacting with the database through the command line, start a session with `psql -d nought`
 
 `\l` = list all databases in postgresql
 `\c` = move inside a database
@@ -18,3 +18,5 @@ Start interacting with the database through the command line, start a session wi
 `CREATE TABLE todo;` = creates a table called 'todo'
 
 We also defined a schema (defined in creation, see `database.sql`)
+
+We built a RESTful API for the database with add, list, update, and delete commands.
--- server/index.js
+++ server/index.js
@@ -27,15 +27,57 @@ app.post('/todos', async (req, res) => {
 })
 
 // Get all todos
-app.get('/culture', async (req, res) => {
+app.get('/todos', async (req, res) => {
 	try {
-		//
+		const allTodos = await pool.query('SELECT * FROM todo')
+		res.json(allTodos.rows)
 	} catch (err) {
-		//
+		console.error(err.message)
+	}
+})
+
+// Get a todo
+app.get('/todos/:id', async (req, res) => {
+	try {
+		const { id } = req.params
+		const todo = await pool.query('SELECT * FROM todo WHERE todo_id = $1', [
+			id,
+		])
+		res.json(todo.rows[0])
+	} catch (error) {
+		console.error(error.message)
 	}
 })
 
 // Update a todo
+app.put('/todos/:id', async (req, res) => {
+	try {
+		const { id } = req.params
+		const { description } = req.body
+		const updateTodo = await pool.query(
+			'UPDATE todo SET description = $1 WHERE todo_id = $2',
+			[description, id]
+		)
+
+		res.json('Todo was updated')
+	} catch (err) {
+		console.error(err.message)
+	}
+})
+
+// Delete a todo
+app.delete('/todos/:id', async (req, res) => {
+	try {
+		const { id } = req.params
+		const deleteTodo = await pool.query(
+			'DELETE FROM todo WHERE todo_id = $1',
+			[id]
+		)
+		res.json('Todo was deleted')
+	} catch (err) {
+		console.error(error.message)
+	}
+})
 
 app.listen(5000, () => {
 	console.log('server has started on port 5000')
